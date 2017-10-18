b1fc590b61f6439beed98bcec2422b98c34be903
SUKAI at home <huangsukai1997@gmail.com>
2017-10-18 22:55:39 +1100

    finish all part and remove comments in other files

--- src/Bot/Blue.hs
+++ src/Bot/Blue.hs
@@ -1,12 +1,91 @@
 -- Assignment completed by
--- Name    :
--- UID     :
--- Tutor   :
--- Lab Time: 
-
+-- Name    : SUKAI HUANG
+-- UID     : u6492211
+-- Tutor   : Debashish Chakraborty
+-- Lab Time: Thursday 3:00 pm
 module Bot.Blue where
-    
-import Data.Board
 
+import Data.Board
+import Data.Player
+
+
+--Alpha-Beta-Decision(state)
 makeMove :: Board -> LookAhead -> Int
-makeMove = undefined
+makeMove initboard lookahead= decision initboard
+    where
+        -- better indexlist
+        width :: Int
+        width = fst $ dimension initboard
+
+        indexList :: [Int]
+        indexList = [4,3..1] ++ [5..width]
+
+
+        --Max-Value function
+        maxValue :: Board -> Int -> Int -> Int -> Int
+        maxValue board1 alpha beta depth
+            | turn (updateBoard board1 1)== Finished = heuristicValue (updateBoard board1 1)
+            | depth == 0 = heuristicValue board1
+            | otherwise = maxfoldl (\x1 x2 -> max x1 x2) (-1000) indexList
+                where
+                    maxfoldl ::  (Int -> Int -> Int) -> Int -> [Int] -> Int
+                    maxfoldl _ x [] = x
+                    maxfoldl f x [m] = (f x (minValue (updateBoard board1 m) alpha beta (depth -1)))
+                    maxfoldl f x (y:ys:yss)
+                        | f x (minValue (updateBoard board1 y) alpha beta (depth -1)) >= beta = f x (minValue (updateBoard board1 y) alpha beta (depth -1))
+                        | otherwise = if
+                                        (f x (minValue (updateBoard board1 y) alpha beta (depth -1))) > alpha
+                                        then maxfoldl f (f (f x (minValue (updateBoard board1 y) alpha beta (depth -1))) (minValue (updateBoard board1 ys) (f x (minValue (updateBoard board1 y) alpha beta (depth -1))) beta  (depth -1))) yss
+                                        else maxfoldl f (f (f x (minValue (updateBoard board1 y) alpha beta (depth -1))) (minValue (updateBoard board1 ys) alpha beta (depth -1))) yss
+
+
+
+        --Min-Value function
+        minValue :: Board -> Int -> Int -> Int -> Int
+        minValue board2 alpha beta depth
+            | turn (updateBoard board2 1)== Finished = heuristicValue (updateBoard board2 1)
+            | depth == 0 = heuristicValue board2
+            | otherwise  = minfoldl  (\m1 m2 -> min m1 m2) (1000) indexList
+                where
+                    minfoldl ::  (Int -> Int -> Int) -> Int -> [Int] -> Int
+                    minfoldl _ x [] = x
+                    minfoldl f x [m] = (f x (maxValue (updateBoard board2 m) alpha beta (depth -1)))
+                    minfoldl f x (y:ys:yss)
+                        | f x (maxValue (updateBoard board2 y) alpha beta (depth-1) ) <= alpha = f x (maxValue (updateBoard board2 y) alpha beta (depth -1))
+                        | otherwise = if
+                                        (f x (maxValue (updateBoard board2 y) alpha beta (depth -1))) < beta
+                                        then minfoldl f (f (f x (maxValue (updateBoard board2 y) alpha beta (depth -1))) (maxValue (updateBoard board2 ys) alpha (f x (maxValue (updateBoard board2 y) alpha beta (depth -1)))(depth -1) )) yss
+                                        else minfoldl f (f (f x (maxValue (updateBoard board2 y) alpha beta (depth -1))) (maxValue (updateBoard board2 ys) alpha beta (depth -1))) yss
+
+
+        --Actions (state) -- maximizing Min-Value(Result(a,state))
+        decision :: Board -> Int
+        decision board1 = choose $ zip (map (\x -> minValue x (-1000) 1000 (lookahead -1) ) (successor board1)) indexList
+            where
+                choose :: [(Int,Int)] -> Int
+                choose [] = 2
+                choose (x:[]) = snd x
+                choose (x:xs:xss)
+                    | (fst x) >= (fst xs) && (validMove board1 (snd x))= choose (x:xss)
+                    | otherwise = choose (xs:xss)
+
+
+        --successors function
+
+        successor :: Board -> [Board]
+        successor board1 = map' (\m -> updateBoard board1 m) indexList
+            where
+                map':: (Int -> b) -> [Int] -> [b]
+                map' _ [] = []
+                map' f (x:xs)
+                    | turn board1 == Finished = []
+                    | turn (updateBoard board1 x) == Finished = [f x]
+                    | otherwise = f x : map' f xs
+
+
+
+        heuristicValue :: Board -> Int -- since it is zero-sum game, the heuristicValue will be the sum of the value
+        heuristicValue b = blueScore b - redScore b
+
+
+
--- src/Bot/Red.hs
+++ src/Bot/Red.hs
@@ -1,6 +1,71 @@
 module Bot.Red where
 
+
 import Data.Board
+import Data.Player
+
+type ABValue = (Max,Min) -- this is the ab pruning tuple
+
+type Max = Int
+type Min = Int
+type MiniMax = Int -- this is the minimax value for that board state
 
 makeMove :: Board -> LookAhead -> Int
-makeMove = undefined
+makeMove currentBoard look = decision  (zip (map' (\x -> miniMax x (look-1)) (map (\y -> updateBoard currentBoard y) indexList)) (indexList))
+
+
+    where
+        map' :: (a->b) -> [a] -> [b]
+        map' _ [] = []
+        map' f (x:xs)
+            | True = f x : map f xs
+            | otherwise = []
+        -- decision function
+        decision :: (Ord a ) => [(a,Int)] -> Int
+        decision [] = 1
+        decision (x:[]) = snd x
+        decision (x:xs:xss)
+            | fst x >= fst xs && (validMove currentBoard (snd x))= decision (x:xss)
+            | otherwise = decision (xs:xss)
+        --
+        -- better indexlist
+        width = fst $ dimension currentBoard
+        firstList = reverse [1 .. (width `div` 2)]
+        secondList = [(width `div` 2 + 1 ) .. width ]
+        combine :: [Int] -> [Int] -> [Int]
+        combine lst1 [] = lst1
+        combine [] lst2 = lst2
+        combine (x:xs) (y:ys) = x:y:combine xs ys
+        indexList =  combine firstList secondList
+
+
+
+
+
+
+        miniMax :: Board -> LookAhead -> MiniMax
+        miniMax board1 lk
+            | lk == 0 = case board1 of
+                Board _ _ _ BlueBot _ _-> foldl' (\x y -> min x $ heuristicValue (updateBoard board1 y)) (1000) indexList
+                Board _ _ _ RedBot _ _ -> foldl' (\x y -> max x $ heuristicValue (updateBoard board1 y)) (-1000) indexList
+                Board _ _ _ Finished _ _ -> heuristicValue board1
+            | lk > 0 = case board1 of
+                    Board _ _ _ BlueBot _ _-> foldl' (\m n -> min m $ miniMax n (lk-1)) (1000) (map (\y -> updateBoard board1 y) indexList)
+                    Board _ _ _ RedBot _ _ -> foldl' (\m n -> max m $ miniMax n (lk-1)) (-1000) (map (\y -> updateBoard board1 y) indexList)
+                    Board _ _ _ Finished _ _ -> heuristicValue board1
+            | otherwise = error"lk bigger than 0"
+
+            where
+                -- foldl special
+                foldl' :: (a->b->a) -> a -> [b] -> a
+                foldl' _ x [] = x
+                foldl' f x (y:ys) = foldl' f (f x y) ys
+
+
+
+
+        heuristicValue :: Board -> MiniMax -- since it is zero-sum game, the heuristicValue will be the sum of the value
+        heuristicValue b =  redScore b - blueScore b
+
+
+
