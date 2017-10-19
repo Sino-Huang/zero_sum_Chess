eb84f612b58fe727c48ce1aaaf05fcf6b086c405
SUKAI at home <huangsukai1997@gmail.com>
2017-10-19 15:59:13 +1100

    change heuristicValue function to fit the tournament

--- src/Bot/Blue.hs
+++ src/Bot/Blue.hs
@@ -18,7 +18,7 @@ makeMove initboard lookahead= decision initboard
         width = fst $ dimension initboard
 
         indexList :: [Int]
-        indexList = [1..width]
+        indexList = [(width `div` 2 +1) .. width] ++ [1.. (width `div` 2)]
 
 
         --alphabetapruning function -- output the heuristic value
@@ -37,27 +37,28 @@ makeMove initboard lookahead= decision initboard
                 maxfoldl _ preval [] = preval
                 maxfoldl f preval (x:xs)
                               | preval >= beta = preval
-                              | updateBoard bo x == bo = maxfoldl f preval xs
                               | otherwise = maxfoldl f (f preval
                                                                ( alphabetapruning (updateBoard bo x) (depth - 1) (max alpha preval) beta False ))
                                                                 xs
+
 
 
                 minfoldl :: (Int -> Int -> Int ) -> Int -> [Int] -> Int
                 minfoldl _ preval [] = preval
                 minfoldl f preval (x:xs)
                                | preval <= alpha = preval
-                               | updateBoard bo x == bo = minfoldl f preval xs
                                | otherwise = minfoldl f
                                                             (f preval
                                                                ( alphabetapruning (updateBoard bo x) (depth - 1) alpha (min preval beta) True ))
                                                                  xs
 
 
+
         --Actions (state) -- maximizing Min-Value(Result(a,state))
         decision :: Board -> Int
         decision board1 = choose $ zip (map (\x -> alphabetapruning x lookahead (-1000) 1000 False ) (map (\x -> updateBoard board1 x) indexList )) indexList
             where
+
                 choose :: [(Int,Int)] -> Int
                 choose [] = error"something wrong"
                 choose (x:[]) = snd x
@@ -69,7 +70,10 @@ makeMove initboard lookahead= decision initboard
 
 
         heuristicValue :: Board -> Int -- since it is zero-sum game, the heuristicValue will be the sum of the value
-        heuristicValue b = blueScore b - (redScore b * 2)
+        heuristicValue b
+            | turn initboard == BlueBot = blueScore b - redScore b
+            | turn initboard == RedBot = redScore b - blueScore b
+            | otherwise = error"ending already"
 
 
 
