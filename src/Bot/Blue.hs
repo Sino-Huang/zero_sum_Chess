b3ee8a21c5d3196fc31f59ecfd3cc1d26435e8d9
SUKAI at home <huangsukai1997@gmail.com>
2017-10-19 16:40:27 +1100

    change choose function

--- src/Bot/Blue.hs
+++ src/Bot/Blue.hs
@@ -8,7 +8,6 @@ module Bot.Blue where
 import Data.Board
 import Data.Player
 
-
 --Alpha-Beta-Decision(state)
 makeMove :: Board -> LookAhead -> Int
 makeMove initboard lookahead= decision initboard
@@ -20,18 +19,14 @@ makeMove initboard lookahead= decision initboard
         indexList :: [Int]
         indexList = [(width `div` 2 +1) .. width] ++ [1.. (width `div` 2)]
 
-
         --alphabetapruning function -- output the heuristic value
         alphabetapruning :: Board -> Int -> Int -> Int -> Bool -> Int
         alphabetapruning bo depth alpha beta whethermax
             | depth == 0 || turn bo == Finished = heuristicValue bo
             | otherwise = case whethermax of
-                True  -> maxfoldl (\preVal newVal -> max preVal  newVal) (-1000) indexList
+                True  -> maxfoldl max (-1000) indexList
 
-
-
-
-                False -> minfoldl (\preVal newVal -> min preVal newVal) (1000) indexList
+                False -> minfoldl min (1000) indexList
             where
                 maxfoldl :: (Int -> Int -> Int ) -> Int -> [Int] -> Int
                 maxfoldl _ preval [] = preval
@@ -40,8 +35,6 @@ makeMove initboard lookahead= decision initboard
                               | otherwise = maxfoldl f (f preval
                                                                ( alphabetapruning (updateBoard bo x) (depth - 1) (max alpha preval) beta False ))
                                                                 xs
-
-
 
                 minfoldl :: (Int -> Int -> Int ) -> Int -> [Int] -> Int
                 minfoldl _ preval [] = preval
@@ -52,28 +45,21 @@ makeMove initboard lookahead= decision initboard
                                                                ( alphabetapruning (updateBoard bo x) (depth - 1) alpha (min preval beta) True ))
                                                                  xs
 
-
-
         --Actions (state) -- maximizing Min-Value(Result(a,state))
         decision :: Board -> Int
         decision board1 = choose $ zip (map (\x -> alphabetapruning x lookahead (-1000) 1000 False ) (map (\x -> updateBoard board1 x) indexList )) indexList
             where
 
                 choose :: [(Int,Int)] -> Int
-                choose [] = error"something wrong"
-                choose (x:[]) = snd x
                 choose (x:xs:xss)
-                    | (fst x) >= (fst xs) && (validMove board1 (snd x))= choose (x:xss)
+                    | (fst x) >= (fst xs) && (updateBoard board1 (snd x) /= board1)= choose (x:xss)
                     | otherwise = choose (xs:xss)
-
-
-
+                choose (x:[]) = snd x
+                choose [] = error"something wrong"
 
         heuristicValue :: Board -> Int -- since it is zero-sum game, the heuristicValue will be the sum of the value
         heuristicValue b
             | turn initboard == BlueBot = blueScore b - redScore b
-            | turn initboard == RedBot = redScore b - blueScore b
-            | otherwise = error"ending already"
-
+            | otherwise  = redScore b - blueScore b
 
 
--- src/Bot/Red.hs
+++ src/Bot/Red.hs
@@ -1,9 +1,6 @@
 module Bot.Red where
-
-
 import Data.Board
 import Data.Player
-
 
 --Alpha-Beta-Decision(state)
 makeMove :: Board -> LookAhead -> Int
@@ -14,58 +11,51 @@ makeMove initboard lookahead= decision initboard
         width = fst $ dimension initboard
 
         indexList :: [Int]
-        indexList = [1..width]
-
+        indexList = [(width `div` 2 +1) .. width] ++ [1.. (width `div` 2)]
 
         --alphabetapruning function -- output the heuristic value
         alphabetapruning :: Board -> Int -> Int -> Int -> Bool -> Int
         alphabetapruning bo depth alpha beta whethermax
             | depth == 0 || turn bo == Finished = heuristicValue bo
             | otherwise = case whethermax of
-                True  -> maxfoldl (\preVal newVal -> max preVal  newVal) (-1000) indexList
+                True  -> maxfoldl max (-1000) indexList
 
-
-
-
-                False -> minfoldl (\preVal newVal -> min preVal newVal) (1000) indexList
+                False -> minfoldl min (1000) indexList
             where
                 maxfoldl :: (Int -> Int -> Int ) -> Int -> [Int] -> Int
                 maxfoldl _ preval [] = preval
                 maxfoldl f preval (x:xs)
                               | preval >= beta = preval
-                              | updateBoard bo x == bo = maxfoldl f preval xs
                               | otherwise = maxfoldl f (f preval
                                                                ( alphabetapruning (updateBoard bo x) (depth - 1) (max alpha preval) beta False ))
                                                                 xs
-
 
                 minfoldl :: (Int -> Int -> Int ) -> Int -> [Int] -> Int
                 minfoldl _ preval [] = preval
                 minfoldl f preval (x:xs)
                                | preval <= alpha = preval
-                               | updateBoard bo x == bo = minfoldl f preval xs
                                | otherwise = minfoldl f
                                                             (f preval
                                                                ( alphabetapruning (updateBoard bo x) (depth - 1) alpha (min preval beta) True ))
                                                                  xs
 
-
         --Actions (state) -- maximizing Min-Value(Result(a,state))
         decision :: Board -> Int
         decision board1 = choose $ zip (map (\x -> alphabetapruning x lookahead (-1000) 1000 False ) (map (\x -> updateBoard board1 x) indexList )) indexList
             where
+
                 choose :: [(Int,Int)] -> Int
-                choose [] = error"something wrong"
-                choose (x:[]) = snd x
                 choose (x:xs:xss)
-                    | (fst x) >= (fst xs) && (validMove board1 (snd x))= choose (x:xss)
+                    | (fst x) >= (fst xs) && (updateBoard board1 (snd x) /= board1)= choose (x:xss)
                     | otherwise = choose (xs:xss)
-
-
-
+                choose (x:[]) = snd x
+                choose [] = error"something wrong"
 
         heuristicValue :: Board -> Int -- since it is zero-sum game, the heuristicValue will be the sum of the value
-        heuristicValue b = redScore b - (blueScore b *2)
+        heuristicValue b
+            | turn initboard == BlueBot = blueScore b - redScore b
+            | otherwise  = redScore b - blueScore b
+
 
 
 
